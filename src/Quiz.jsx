74b8981a75a68f5b34b2ce82841c0b05835ae468
Maciej Gajda <gajdam11@gmail.com>
2024-05-31 12:30:07 +0200

    added: question submit btn, modified: show the right answer

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -7,13 +7,14 @@ const Quiz = ({ data, onNewFile }) => {
   const [selectedOption, setSelectedOption] = useState(null);
   const [score, setScore] = useState(0);
   const [showScore, setShowScore] = useState(false);
+  const [hasAnswered, setHasAnswered] = useState(false);
 
   const handleOptionClick = (optionIndex) => {
     setSelectedOption(optionIndex);
     if (data[currentQuestionIndex].options[optionIndex].isCorrect) {
       setScore(score + 1);
     }
-    setTimeout(nextQuestion, 2000);
+    setHasAnswered(true);
   };
 
   const nextQuestion = () => {
@@ -22,6 +23,7 @@ const Quiz = ({ data, onNewFile }) => {
     if (currentQuestionIndex >= data.length - 1) {
       setShowScore(true);
     }
+    setHasAnswered(false);
   };
 
   const handlePlayAgain = () => {
@@ -35,23 +37,31 @@ const Quiz = ({ data, onNewFile }) => {
     <div className="quiz-container">
       {showScore ? (
         <div>
-          <div className="score-display">You scored {score} out of {data.length}</div>
+          <div className="score-display">You got {Math.round((score / data.length) * 100)} %</div>
           <button onClick={handlePlayAgain} className="option-button">Play Again</button>
           <button onClick={onNewFile} className="option-button">New File</button>
         </div>
       ) : (
-        <div>
-          <div className="question">{data[currentQuestionIndex].question}</div>
-          {data[currentQuestionIndex].options.map((option, index) => (
-            <button
-              key={index}
-              onClick={() => handleOptionClick(index)}
-              className={`option-button ${selectedOption === index ? (option.isCorrect ? 'correct' : 'incorrect') : ''}`}
-            >
-              {option.option}
-            </button>
-          ))}
-        </div>
+          <div>
+            <div className="question-display">{data[currentQuestionIndex].question}</div>
+            <div>
+              {data[currentQuestionIndex].options.map((option, index) => (
+                <button
+                  key={index}
+                  onClick={() => handleOptionClick(index)}
+                  className={`
+          option-button 
+          ${selectedOption === index ? (option.isCorrect ? 'correct' : 'wrong') : ''}
+          ${hasAnswered && option.isCorrect ? 'correct' : ''}
+        `}
+                  disabled={hasAnswered}
+                >
+                  {option.option}
+                </button>
+              ))}
+            </div>
+            <button onClick={nextQuestion} className="option-button" disabled={!hasAnswered}>Next Question</button>
+          </div>
       )}
     </div>
   );
